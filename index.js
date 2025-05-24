90cf8cf0ad6d3fdcba7b183228c879d5e9ac0a03
akukamil <chamilioner_kid@mail.ru>
2025-05-24 23:03:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7911,7 +7911,7 @@ async function init_game_env(lang) {
 	//из локального хранилища
 	my_data.board_id = safe_ls('pool_board_id')||1;
 	my_data.cue_id = other_data?.cue_id ||4;
-	my_data.cue_resource = other_data?.cue_data||[9,100,50,50,50,0,0,0];
+	my_data.cue_resource = other_data?.cue_data||[9,100,50,50,150,0,0,0];
 
 	//правильно определяем аватарку
 	if (other_data?.pic_url && other_data.pic_url.includes('mavatar'))
