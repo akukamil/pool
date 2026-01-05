2768be3fb7fda4279cb7da4364e7309a286915fe
akukamil <chamilioner_kid@mail.ru>
2026-01-05 16:50:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2337,7 +2337,7 @@ auth2={
 
 		//сохраняем этот uid в локальном хранилище
 		try {
-			localStorage.setItem('poker_uid', uid);
+			localStorage.setItem('pool_uid', uid);
 		} catch (e) {alert(e)}
 
 		return uid;
@@ -2398,7 +2398,7 @@ auth2={
 		let local_uid = null;
 
 		try {
-			local_uid = localStorage.getItem('poker_uid');
+			local_uid = localStorage.getItem('pool_uid');
 		} catch (e) {alert(e)}
 
 		if (local_uid !== null) return local_uid;
